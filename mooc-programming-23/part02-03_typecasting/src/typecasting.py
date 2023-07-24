4521fa31358540c0718be74ba94fcceb1d0a7c60
Andoni Izaguirre <48142479+andonizaguirre@users.noreply.github.com>
2023-07-23 23:47:17 -0500

    Part 2, 1-5

---
+++ mooc-programming-23/part02-03_typecasting/src/typecasting.py
@@ -0,0 +1,4 @@
+# Write your solution here
+floatNumber = float(input("Please type in a number"))
+print("Integer part: " + str(int(floatNumber)))
+print("Decimal part: " + str(floatNumber - int(floatNumber)))
